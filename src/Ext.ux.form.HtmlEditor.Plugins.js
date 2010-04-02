4a06a5c85c1b63f22342a8bec8b427e526c86549
Shea Frederick <justarock@gmail.com>
2010-04-02 11:56:20 -0400

    added some documentation

--- src/Ext.ux.form.HtmlEditor.Plugins.js
+++ src/Ext.ux.form.HtmlEditor.Plugins.js
@@ -2,6 +2,17 @@
  * @author Shea Frederick - http://www.vinylfox.com
  * @class Ext.ux.form.HtmlEditor.plugins
  * <p>A convenience function that returns a standard set of HtmlEditor buttons.</p>
+ * <p>Sample usage:</p>
+ * <pre><code>
+    new Ext.FormPanel({
+        ...
+        items : [{
+            ...
+            xtype           : "htmleditor",
+            plugins         : Ext.ux.form.HtmlEditor.plugins()
+        }]
+    });
+ * </code></pre>
  */
 Ext.ux.form.HtmlEditor.plugins = function(){
     return [
